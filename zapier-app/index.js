dddcff9269bd475381700e523cbd9ca27c0ad16c
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2020-02-07 00:41:48 -0600

    (zapier) Update app

--- zapier-app/index.js
+++ zapier-app/index.js
@@ -1,29 +1,19 @@
-const recipe = require('./triggers/recipe');
+const platformVersion = require('zapier-platform-core').version;
 
-// Now we can roll up all our behaviors in an App.
+const oscarsWinnerTrigger = require('./triggers/oscars-winner');
+const version = require('./package.json').version;
+
 const App = {
-  // This is just shorthand to reference the installed dependencies you have. Zapier will
-  // need to know these before we can upload
-  version: require('./package.json').version,
-  platformVersion: require('zapier-platform-core').version,
-
+  version,
+  platformVersion,
   beforeRequest: [],
-
   afterResponse: [],
-
   resources: {},
-
-  // If you want your trigger to show up, you better include it here!
   triggers: {
-    [recipe.key]: recipe
+    [oscarsWinnerTrigger.key]: oscarsWinnerTrigger
   },
-
-  // If you want your searches to show up, you better include it here!
   searches: {},
-
-  // If you want your creates to show up, you better include it here!
   creates: {}
 };
 
-// Finally, export the app.
 module.exports = App;
