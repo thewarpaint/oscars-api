30b20ee5b1af3d738c589c775dbf1fefdfd61df8
Eduardo Garcia <eduardogarcia.xyz+gh@gmail.com>
2020-02-09 01:07:04 -0600

    (api) Add create and delete hook logic

--- webhooks-api/index.js
+++ webhooks-api/index.js
@@ -1,11 +1,18 @@
+const fs = require('fs');
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
+const uuid = require('uuid/v4');
 
 const dummyNominees = require('./dummy-nominees');
 
 const app = new Koa();
 const router = new Router();
+
+const dbFile = 'db.json';
+const dbFileOptions = {
+  encoding: 'utf8'
+};
 
 app.use(bodyParser());
 
@@ -19,20 +26,61 @@ router.get('/api/v1/nominees', async (ctx) => {
   ctx.body = dummyNominees;
 });
 
+// https://open.spotify.com/track/13XHuE00ElL5thSxkaEXxK
 router.post('/api/v1/hooks', async (ctx) => {
-  console.log('Create hook', ctx.request.body);
+  const { url } = ctx.request.body;
+
+  const newHook = {
+    url
+  };
+
+  const newHookId = uuid();
+
+  console.log(`Creating hook for url=${url} with hookId=${newHookId}`);
+
+  const db = getDatabase();
+
+  db[newHookId] = newHook;
+  writeDatabase(db);
+
+  console.log(`hookId=${newHookId} was created successfully`);
 
   ctx.body = {
-    id: 'zapier',
+    id: newHookId,
     status: 'success'
   };
 });
 
-router.delete('/api/v1/hooks/zapier', async (ctx) => {
-  console.log('Delete hook', ctx.request.body);
+router.delete('/api/v1/hooks/:hookId', async (ctx) => {
+  const { hookId } = ctx.params;
+
+  console.log(`Deleting hookId=${hookId}`);
+
+  const db = getDatabase();
+
+  if (db[hookId]) {
+    delete db[hookId];
+    writeDatabase(db);
+
+    console.log(`hookId=${hookId} was deleted successfully`);
+  } else {
+    console.log(`hookId=${hookId} wasn't found in the database, returning 404`);
+
+    ctx.status = 404;
+  }
 
   ctx.body = {};
 });
 
 app.use(router.routes());
 app.listen(3000);
+
+function getDatabase() {
+  const dbText = fs.readFileSync(dbFile, dbFileOptions);
+
+  return JSON.parse(dbText);
+}
+
+function writeDatabase(data) {
+  fs.writeFileSync(dbFile, JSON.stringify(data, null, 2), dbFileOptions);
+}
